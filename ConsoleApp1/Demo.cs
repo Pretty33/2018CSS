b15ed9bd9eff87f7b8a52f9a6bda4ca5d013f866
Pretty33 <321838579@qq.com>
2019-01-07 00:03:11 +0800

    添加项目文件。

---
+++ ConsoleApp1/Demo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Demo
+    {
+        /// <summary>
+        /// 完美数
+        /// </summary>
+        /// <param name="Max"></param>
+        /// <returns></returns>
+        public int[] Perfect(int Max)
+        {
+            List<int> result = new List<int>();
+
+            for (int z = 2; z < Max; z++)
+            {
+                int sum = 0;
+                for (int j = z - 1; j > 0; j--)
+                {
+                    if (z % j == 0)
+                        sum += j;
+
+                    if (j == 1)
+                    {
+                        if (sum == z)
+                        {
+                            result.Add(z);
+                        }
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <param name="y">传入未排序的数组</param>
+        /// <returns>排序后的数组<</returns>
+        public int[] InsertionSort(int[] y)
+        {
+            for (int k = 0; k < y.Length - 1; k++)
+            {
+                int b = y[k + 1];
+                int j = k;
+                while (j >= 0 && y[j] > b)
+                {
+                    y[j + 1]= y[j];
+                    j--;
+                }
+                y[j + 1] = b;
+            }
+            return y;
+        }
+
+        /// <summary>
+        /// 计算斐波那契数
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public int Fabonacci(int a)
+        {
+            int result = 1;
+            if (a == 0 || a == 1)
+                result = 1;
+            else result = Fabonacci(a - 1) + Fabonacci(a - 2);
+            return result;
+            
+        }
+
+        /// <summary>
+        /// 计算index的阶乘
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public int Factoria(int index)
+        {
+            int result = 1;
+            if (index == 0)
+                return 1;
+            result = index * Factoria(index - 1);
+            while (index > 0)
+            {
+                result *= index;
+                index--;
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   class Program
+    {
+        static Demo demo = new Demo();
+
+        static void Main(string[] args)
+        {
+            int[] res = demo.Perfect(10000); //调用完美数的方法
+            foreach(var z in res)
+            {
+                Console.WriteLine("这是一个完美数:"+ z );
+            }
+
+            //排序
+            int[] yu = { 5, 4, 3, 2, 1 };
+            int[] l = demo.InsertionSort(yu);
+            for (int k= 0; k < l.Length;k++)
+            {
+                Console.WriteLine(l[k]);
+            }
+
+
+            //斐波那契数
+            int ba = demo.Fabonacci(10);
+            Console.WriteLine(ba);
+
+
+            //index的阶乘
+            int fa = demo.Factoria(5);
+            Console.WriteLine(fa);
+
+            DecryptTheQQNumber("321568897");//解密QQ号
+            Console.WriteLine(DecryptionPalindrome(Console.ReadLine()));//回文
+            Console.ReadKey();
+        }
+
+
+        #region 解密QQ号
+        public static void DecryptTheQQNumber(string QQNumber)
+        {
+            //声明一个队列
+            Queue clear = new Queue();
+            //将传递过来的参数复制给队列clear
+            foreach (var item in QQNumber)
+            {
+                clear.Enqueue(item);
+            }
+            //进行循环
+            for (int i = 0; i < QQNumber.Length; i++)
+            {
+                //如果队列只剩下一位，直接输出，并跳出循环
+                if (clear.Count == 1)
+                {
+                    Console.Write(clear.Dequeue());
+                    break;
+                }
+                //输出队列的第一位
+                Console.Write(clear.Dequeue());
+                //将队列的第一位放到最后
+                clear.Enqueue(clear.Dequeue());
+            }
+        }
+        #endregion
+
+        #region 解密回文
+        public static string DecryptionPalindrome(string chars)
+        {
+            //声明一个栈
+            Stack stack = new Stack();
+            Stack mirroring = new Stack();
+            //将传递过来的string型注入栈中
+            foreach (var item in chars)
+            {
+                stack.Push(item);
+            }
+            //将栈的一半值赋值给另一个栈
+            for (int i = 0; i < chars.Length / 2; i++)
+            {
+                mirroring.Push(stack.Pop());
+            }
+            //判断chars的原始数据长度是奇数还是偶数
+            if (stack.Count != mirroring.Count)
+            {
+                stack.Pop();
+            }
+            //通过长度去判断两个栈的值是否相等
+            for (int i = 0; i < stack.Count + mirroring.Count; i++)
+            {
+                if (mirroring.Pop().ToString() != stack.Pop().ToString())
+                {
+                    return "这不是回文";
+                }
+            }
+            return "这是回文";
+
+        }
+        #endregion
+
+    }
+}
