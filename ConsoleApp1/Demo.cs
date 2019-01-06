77d4c350b9ad0190bd950bf999b6a92f5caae268
Pretty33 <321838579@qq.com>
2019-01-07 00:15:29 +0800

    20190106

--- ConsoleApp1/Demo.cs
+++ ConsoleApp1/Demo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -91,7 +92,5 @@ namespace ConsoleApp1
             }
             return result;
         }
-
-
     }
 }
